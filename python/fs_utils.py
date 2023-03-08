111bb68b5a5794a8fc7cc20e412e0e956112fe13
Marek Rogalski <mafikpl@gmail.com>
2023-03-08 12:09:02 +0100

    Use win32-compatible temp directory

--- python/fs_utils.py
+++ python/fs_utils.py
@@ -1,10 +1,11 @@
 '''Utilities for operating on filesystem.'''
 
 from pathlib import Path
+import tempfile
 
 project_root = Path(__file__).resolve().parents[1]
 project_name = Path(project_root).name.lower()
 
 # Alternative location (only works on systemd-compatible systems)
 # f'/run/user/{os.geteuid()}/{fs_utils.project_name}/'
-project_tmp_dir = Path(f'/tmp/{project_name}/')
+project_tmp_dir = Path(tempfile.gettempdir()) / project_name

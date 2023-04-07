75d5acc347926da17fbe16f9a5052ce5dc02b2ea
Marek Rogalski <mafikpl@gmail.com>
2023-04-07 17:57:01 +0200

    Record text-related low-priority TODOs

--- src/font.h
+++ src/font.h
@@ -18,10 +18,14 @@ struct Font {
 
   static std::unique_ptr<Font> Make(float letter_size_mm);
 
+  // TODO: If this causes performance issues, cache text shaping / SkTextBlob
+  // results somehow
   void DrawText(SkCanvas &canvas, std::string_view text, SkPaint &paint);
   float MeasureText(std::string_view text);
   float PositionFromIndex(std::string_view text, int index);
   int IndexFromPosition(std::string_view text, float x);
+  // TODO: If this causes performance issues, use ICU directly rather than going
+  // through SkShaper
   int PrevIndex(std::string_view text, int index);
   int NextIndex(std::string_view text, int index);
 };

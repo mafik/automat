2eaed4a14effddbc2cb01a7d0c2a5d5ae0ec765f
Marek Rogalski <mafikpl@gmail.com>
2023-04-19 09:14:10 +0200

    Support for color mixing

--- src/color.h
+++ src/color.h
@@ -4,10 +4,14 @@
 
 namespace automaton::color {
 
+// Nice article about color spaces: https://ciechanow.ski/color-spaces/
+
 SkColor SetAlpha(SkColor color, uint8_t alpha);
 
 SkColor SetAlpha(SkColor color, float alpha_01);
 
 SkColor AdjustLightness(SkColor color, float adjust_percent);
+
+SkColor MixColors(SkColor zero, SkColor one, float ratio);
 
 } // namespace automaton::color

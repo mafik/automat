d251fadd7d755a8bcffff503e8dc97e9b2d36661
Marek Rogalski <mafikpl@gmail.com>
2023-05-11 16:31:06 +0200

    Proper declaration of the "target" argument of Increment

--- src/library_increment.h
+++ src/library_increment.h
@@ -9,6 +9,7 @@ struct Increment : Object {
   static Argument target_arg;
   string_view Name() const override;
   std::unique_ptr<Object> Clone() const override;
+  void Args(std::function<void(Argument &)> cb) override { cb(target_arg); }
   void Run(Location &h) override;
   void Draw(SkCanvas &canvas, animation::State &animation_state) const override;
   SkPath Shape() const override;

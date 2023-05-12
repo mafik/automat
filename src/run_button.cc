6990147cc458131ecd1fef9f1cf8f709f62e16f5
Marek Rogalski <mafikpl@gmail.com>
2023-05-12 11:51:50 +0200

    Clear error when an object is run

--- src/run_button.cc
+++ src/run_button.cc
@@ -2,8 +2,8 @@
 
 #include "base.h"
 #include "gui_shape_widget.h"
-#include "log.h"
 #include "location.h"
+#include "log.h"
 
 namespace automaton::gui {
 
@@ -11,18 +11,22 @@ constexpr char kPlayShape[] =
     "M-5-8C-5.8-6-5.7 6-5 8-3 7.7 7.5 1.5 9 0 7.5-1.5-3-7.7-5-8Z";
 
 RunButton::RunButton(Location *parent)
-    : Button(parent, MakeShapeWidget(kPlayShape, 0xffffffff)), location(parent) {}
+    : Button(parent, MakeShapeWidget(kPlayShape, 0xffffffff)),
+      location(parent) {}
 
 void RunButton::Activate() {
   if (Filled()) {
-    // TODO: Cancel?
+    // TODO: Cancel.
   } else {
+    // This will destroy a potential object saved in the error so it shouldn't
+    // be automatic. Maybe we could ClearError only if there is no object?
+    // Otherwise we should ask the user.
+    // TODO(after Errors are visualized): Ask the user.
+    location->ClearError();
     location->ScheduleRun();
   }
 }
 
-bool RunButton::Filled() const {
-  return location->run_task.scheduled;
-}
+bool RunButton::Filled() const { return location->run_task.scheduled; }
 
 } // namespace automaton::gui

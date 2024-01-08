91082ed39f269c54dfeac60f9c9fea6de8149dd0
Marek Rogalski <mafikpl@gmail.com>
2024-01-08 14:49:49 +0100

    Icon for the "Next" action

--- src/run_button.cc
+++ src/run_button.cc
@@ -8,6 +8,9 @@
 namespace automat::gui {
 
 constexpr char kPlayShape[] = "M-5-8C-5.8-6-5.7 6-5 8-3 7.7 7.5 1.5 9 0 7.5-1.5-3-7.7-5-8Z";
+constexpr char kNextShape[] =
+    "M-7-8C-7.8-6-7.7 6-7 8-5 7.7 5.5 1.5 7 0Q7-4 6-7.5L8-8Q9-4 9 0 9 4 8 8L6 7.5Q7 4 7 "
+    "0C5.5-1.5-5-7.7-7-8Z";
 
 RunButton::RunButton(Location* parent)
     : Button(MakeShapeWidget(kPlayShape, 0xffffffff)), location(parent) {}

5d98fe4d7a049f0f82391ea27b61b888ebeba8f5
Marek Rogalski <mafikpl@gmail.com>
2024-09-01 12:39:20 +0200

    Event delivery benchmark

---
+++ tests/event_bench.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+if __name__ == '__main__':
+  import subprocess, shutil
+  from pathlib import Path
+
+  root = Path(__file__).parent.parent.resolve()
+  build_path = root / 'build'
+  run_path = root / 'run.py'
+  automat_path = build_path / 'release_automat.exe'
+  test_state_path = root / 'tests' / 'event_bench.json'
+  state_path = build_path / 'automat_state.json'
+
+  subprocess.run(['python', str(run_path), 'link release_automat.exe'])
+  shutil.copy(test_state_path, state_path)
+
+  p = subprocess.Popen([str(automat_path)])
+  p.wait()
+
+  print('Done!')
+
+  # TODO: Analyze resulting `automat_state.json`
+  #    - drop any odd events at the end
+  #    - use the first event as the basis to find expected delivery times
+  #    - calculate the mean squared error

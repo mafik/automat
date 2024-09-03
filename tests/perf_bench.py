637af8eb31c9fe74ca7441baf36ee3ebb68533d8
Marek Rogalski <mafikpl@gmail.com>
2024-09-03 09:34:21 +0200

    Cleaner flamegraphs

--- tests/perf_bench.py
+++ tests/perf_bench.py
@@ -8,9 +8,9 @@ root_path = tests_path.parent
 build_path = root_path / 'build'
 run_path = root_path / 'run.py'
 
-// TODO: turn this into build graph
+# TODO: turn this into build graph
 
 shutil.copy(tests_path / 'perf_bench.json', build_path / 'automat_state.json')
 subprocess.run(['python', str(run_path), 'link debug_automat'], check=True)
-subprocess.run(['time', 'perf',  'record', '--call-graph', 'fp',  'build/debug_automat'])
-subprocess.run('perf script | third_party/FlameGraph/stackcollapse-perf.pl | third_party/FlameGraph/flamegraph.pl --width 1890 > build/flamegraph.svg', shell=True, check=True)
+subprocess.run(['time', 'perf',  'record', '--call-graph', 'fp', '--user-callchains', 'build/debug_automat'])
+subprocess.run('perf script | grep -v "::_" | sed "s/automat:://; s/gui:://; s/std:://; s/library:://" | third_party/FlameGraph/stackcollapse-perf.pl | third_party/FlameGraph/flamegraph.pl --title "Automat Debug" --cp --colors aqua --inverted --bgcolors blue --width 1890 > build/flamegraph.svg', shell=True, check=True)

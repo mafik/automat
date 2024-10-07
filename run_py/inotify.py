362e2391b56cb9747b37ca43b117d00f6045c859
Marek Rogalski <mafikpl@gmail.com>
2024-10-07 12:49:42 +0200

    Remove the pywin32 dependency

--- run_py/inotify.py
+++ run_py/inotify.py
@@ -45,30 +45,79 @@ if sys.platform == 'linux':
   print(buf)
 
 elif sys.platform == 'win32':
-  import win32api, win32file, win32con, threading
+  import threading, ctypes
 
   semaphore = threading.Semaphore(0)
 
-  win32api.SetConsoleCtrlHandler(lambda x: semaphore.release(), True)
+  def SetConsoleCtrlHandler(handler, add):
+    func = ctypes.windll.kernel32.SetConsoleCtrlHandler
+    callback = ctypes.WINFUNCTYPE(ctypes.c_bool, ctypes.c_uint)
+    func.argtypes = [callback, ctypes.c_bool]
+    func.restype = ctypes.c_bool
+    return func(callback(handler), add)
+  
+  def CreateFile(lpFileName:str, dwDesiredAccess:int, dwShareMode:int, lpSecurityAttributes:int, dwCreationDisposition:int, dwFlagsAndAttributes:int, hTemplateFile:int):
+    func = ctypes.windll.kernel32.CreateFileW
+    func.argtypes = [ctypes.c_wchar_p, ctypes.c_uint32, ctypes.c_uint32, ctypes.c_void_p, ctypes.c_uint32, ctypes.c_uint32, ctypes.c_void_p]
+    func.restype = ctypes.c_void_p
+    return func(lpFileName, dwDesiredAccess, dwShareMode, lpSecurityAttributes, dwCreationDisposition, dwFlagsAndAttributes, hTemplateFile)
+  
+  def ReadDirectoryChangesW(hDirectory:int, nBufferLength: int, bWatchSubtree:bool, dwNotifyFilter:int):
+    func = ctypes.windll.kernel32.ReadDirectoryChangesW
+    func.argtypes = [ctypes.c_void_p, ctypes.c_void_p, ctypes.c_uint32, ctypes.c_bool, ctypes.c_uint32, ctypes.c_void_p, ctypes.c_void_p, ctypes.c_void_p]
+    func.restype = ctypes.c_bool
+    buffer = ctypes.create_string_buffer(nBufferLength)
+    bytes_returned = ctypes.c_uint32()
+    overlapped = ctypes.c_void_p()
+    success = func(hDirectory, buffer, nBufferLength, bWatchSubtree, dwNotifyFilter, ctypes.byref(bytes_returned), overlapped, None)
+    if not success:
+      print(ctypes.GetLastError())
+      raise ctypes.WinError()
+    results = []
+    class FILE_NOTIFY_INFORMATION(ctypes.Structure):
+      _fields_ = [
+        ('NextEntryOffset', ctypes.c_uint32),
+        ('Action', ctypes.c_uint32),
+        ('FileNameLength', ctypes.c_uint32)
+      ]
+    offset = 0
+    while offset + ctypes.sizeof(FILE_NOTIFY_INFORMATION) < bytes_returned.value:
+      info = FILE_NOTIFY_INFORMATION.from_buffer(buffer, offset)
+      if info.FileNameLength:
+        first_char = ctypes.addressof(info) + ctypes.sizeof(FILE_NOTIFY_INFORMATION)
+        filename = ctypes.wstring_at(first_char, info.FileNameLength // 2)
+        results.append((info.Action, filename))
+      if info.NextEntryOffset == 0:
+        break
+      offset += info.NextEntryOffset
+    return results
+  
+  GENERIC_READ = 0x80000000
+  FILE_SHARE_READ = 0x00000001
+  FILE_SHARE_WRITE = 0x00000002
+  FILE_SHARE_DELETE = 0x00000004
+  OPEN_EXISTING = 3
+  FILE_FLAG_BACKUP_SEMANTICS = 0x02000000
+  FILE_NOTIFY_CHANGE_LAST_WRITE = 0x00000010
+
+  SetConsoleCtrlHandler(lambda x: semaphore.release(), True)
 
   def watch_thread():
-    handle = win32file.CreateFile(
+    handle = CreateFile(
       watch_dir,
-      win32file.GENERIC_READ,
-      win32file.FILE_SHARE_READ | win32file.FILE_SHARE_WRITE | win32file.FILE_SHARE_DELETE,
+      GENERIC_READ,
+      FILE_SHARE_READ | FILE_SHARE_WRITE | FILE_SHARE_DELETE,
       None,
-      win32file.OPEN_EXISTING,
-      win32file.FILE_FLAG_BACKUP_SEMANTICS,
+      OPEN_EXISTING,
+      FILE_FLAG_BACKUP_SEMANTICS,
       None
     )
 
-    results = win32file.ReadDirectoryChangesW(
+    results = ReadDirectoryChangesW(
       handle,
       1024,
       True,
-      win32con.FILE_NOTIFY_CHANGE_LAST_WRITE,
-      None,
-      None
+      FILE_NOTIFY_CHANGE_LAST_WRITE
     )
 
     for action, file in results:

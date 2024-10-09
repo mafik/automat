7917b2964e9f46a97e127740f273b0aa87f88189
Marek Rogalski <mafikpl@gmail.com>
2024-10-09 14:06:41 +0200

    libgl-dev

--- run_py/debian_deps.py
+++ run_py/debian_deps.py
@@ -16,6 +16,7 @@ path_to_package = {
     "/usr/include/X11/extensions/Xinerama.h": "libxinerama-dev",
     "/usr/include/X11/Xcursor/Xcursor.h": "libxcursor-dev",
     "/usr/include/X11/extensions/XInput2.h": "libxi-dev",
+    "/usr/include/GL/gl.h": "libgl-dev",
 }
 
 

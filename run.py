dd4ff5cfd0ef5983fabbdcfcfa75c72f5f7f8450
Marek Rogalski <mafikpl@gmail.com>
2024-10-09 14:43:45 +0200

    Debug GitHub Actions issues

--- run.py
+++ run.py
@@ -9,4 +9,8 @@ from subprocess import run
 from pathlib import Path
 
 base_dir = Path(__file__).parent
-run(['python', base_dir / 'run_py'] + sys.argv[1:], check=True)
+try:
+  run(['python', base_dir / 'run_py'] + sys.argv[1:], check=True)
+except Exception:
+  print('Script failed. Contents of the build/prefix directory:')
+  run(['find', 'build/prefix'])

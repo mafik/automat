34e245b4ab0496b4f92a6cc5ef21d2847983b603
Marek Rogalski <mafikpl@gmail.com>
2025-02-19 17:53:19 +0100

    Assembler widget

--- src/library_assembler.cc
+++ src/library_assembler.cc
@@ -2,21 +2,33 @@
 // SPDX-License-Identifier: MIT
 #include "library_assembler.hh"
 
+#include <include/effects/SkGradientShader.h>
 #include <llvm/MC/MCCodeEmitter.h>
 #include <llvm/MC/MCInstBuilder.h>
 #include <llvm/MC/MCInstPrinter.h>
 #include <llvm/Support/TargetSelect.h>
 #include <llvm/lib/Target/X86/X86Subtarget.h>
 
+#include <thread>
+
+#include "font.hh"
+#include "svg.hh"
+#include "thread_name.hh"
+
 #if defined __linux__
 #include <signal.h>
 #include <sys/mman.h>  // For mmap related functions and constants
 #include <sys/prctl.h>
 #include <sys/ptrace.h>
+#include <sys/user.h>
 #include <sys/wait.h>
 #endif  // __linux__
 
+#include "embedded.hh"
+#include "global_resources.hh"
+#include "library_instruction.hh"
 #include "llvm_asm.hh"
+#include "random.hh"
 #include "status.hh"
 
 #if defined _WIN32
@@ -63,16 +75,20 @@ static bool SetupSignalHandler() {
   sa.sa_sigaction = (void (*)(int, siginfo_t*, void*))AssemblerSignalHandler;
   sa.sa_flags = SA_SIGINFO | SA_ONSTACK;
 
+  // if (sigaction(SIGTRAP, &sa, NULL) == -1) {
+  //   ERROR << "Failed to set SIGTRAP handler: " << strerror(errno);
+  //   return false;
+  // }
   if (sigaction(SIGSEGV, &sa, NULL) == -1) {
-    perror("Failed to set SIGSEGV handler");
+    ERROR << "Failed to set SIGSEGV handler: " << strerror(errno);
     return false;
   }
   if (sigaction(SIGILL, &sa, NULL) == -1) {
-    perror("Failed to set SIGILL handler");
+    ERROR << "Failed to set SIGILL handler: " << strerror(errno);
     return false;
   }
   if (sigaction(SIGBUS, &sa, NULL) == -1) {
-    perror("Failed to set SIGBUS handler");
+    ERROR << "Failed to set SIGBUS handler: " << strerror(errno);
     return false;
   }
   return true;
@@ -111,6 +127,7 @@ struct Regs {
   ALL_REGS(CB);
   // CB(original_RSP);
 #undef CB
+  uint64_t operator[](int index) { return reinterpret_cast<uint64_t*>(this)[index]; }
 };
 
 Assembler::Assembler(Status& status) {
@@ -126,6 +143,7 @@ Assembler::Assembler(Status& status) {
 #endif  // __linux__
 
   regs = std::make_unique<Regs>();
+  regs->RAX = 0x123456789abcdef0ull;  // dummy value for debuggin
 }
 
 Assembler::~Assembler() {}
@@ -423,23 +441,28 @@ void Assembler::UpdateMachineCode() {
   if (!machine_code_fixups.empty()) {
     ERROR << "Not all fixups were resolved!";
   }
-  string machine_code_str;
-  for (int i = 0; i < machine_code_offset; i++) {
-    machine_code_str += f("%02x ", machine_code.get()[i]);
-    if (i % 16 == 15) {
-      machine_code_str += "\n";
-    }
-  }
-  LOG << machine_code_str;
 
-  string epilogue_prologue_str;
-  for (int i = 0; i < epilogue_prologue.size(); i++) {
-    epilogue_prologue_str += f("%02x ", epilogue_prologue[i]);
-    if (i % 16 == 15) {
-      epilogue_prologue_str += "\n";
+  if constexpr (false) {
+    string machine_code_str;
+    for (int i = 0; i < machine_code_offset; i++) {
+      machine_code_str += f("%02x ", machine_code.get()[i]);
+      if (i % 16 == 15) {
+        machine_code_str += "\n";
+      }
     }
+    LOG << machine_code_str;
   }
-  LOG << epilogue_prologue_str;
+
+  if constexpr (false) {
+    string epilogue_prologue_str;
+    for (int i = 0; i < epilogue_prologue.size(); i++) {
+      epilogue_prologue_str += f("%02x ", epilogue_prologue[i]);
+      if (i % 16 == 15) {
+        epilogue_prologue_str += "\n";
+      }
+    }
+    LOG << epilogue_prologue_str;
+  }
 
 #if defined __linux__
   mprotect(machine_code.get(), kMachineCodeSize, PROT_READ | PROT_EXEC);
@@ -448,57 +471,249 @@ void Assembler::UpdateMachineCode() {
 #endif  // __linux__
 }
 
-std::string Assembler::Widget::Text() const {
-  std::string s = "RAX = ";
-  if (auto obj = object.lock()) {
-    if (auto assembler = std::dynamic_pointer_cast<Assembler>(obj)) {
-      s += f("%lx", assembler->regs->RAX);
-    }
+static void GetRegs(const std::weak_ptr<Object>& object, Regs& regs) {
+  auto obj = object.lock();
+  if (!obj) {
+    ERROR << "Assembler was released";
+    return;
   }
-  return s;
+  auto assembler = std::dynamic_pointer_cast<Assembler>(obj);
+  if (!assembler) {
+    ERROR << "Assembler Widget cannot display non-assembler objects";
+    return;
+  }
+  pid_t pid = assembler->pid;
+  if (pid > 0 && assembler->is_running) {
+    int ret = ptrace(PTRACE_SEIZE, pid, 0, PTRACE_O_TRACESYSGOOD | PTRACE_O_EXITKILL);
+    if (ret != 0) {
+      ERROR << "PTRACE_SEIZE failed: " << strerror(errno);
+      return;
+    }
+    ret = ptrace(PTRACE_INTERRUPT, pid, 0, 0);
+    if (ret != 0) {
+      ERROR << "PTRACE_INTERRUPT failed: " << strerror(errno);
+      return;
+    }
+    int status;
+    ret = waitpid(pid, &status, WSTOPPED);
+    if (ret == -1) {
+      ERROR << "waitpid failed: " << strerror(errno);
+      return;
+    }
+    user_regs_struct linux_regs;
+    ret = ptrace(PTRACE_GETREGS, pid, 0, &linux_regs);
+    if (ret != 0) {
+      ERROR << "PTRACE_GETREGS failed: " << strerror(errno);
+      return;
+    }
+    regs.RAX = linux_regs.rax;
+    // ret = ptrace(PTRACE_CONT, pid, 0, 0);
+    // if (ret != 0) {
+    //   ERROR << "PTRACE_CONT failed: " << strerror(errno);
+    //   return "PTRACE_CONT failed";
+    // }
+    ret = ptrace(PTRACE_DETACH, pid, 0, 0);
+    if (ret != 0) {
+      ERROR << "PTRACE_DETACH failed: " << strerror(errno);
+      return;
+    }
+  } else {
+    regs = *assembler->regs;
+  }
 }
 
 void Assembler::RunMachineCode(library::Instruction* entry_point) {
 #if defined __linux__
-  int STACK_SIZE = 8 * 1024 * 1024;
-  std::vector<char> stack;
-  stack.resize(STACK_SIZE);
-  pid_t v;
   struct ThreadArg {
-    PrologueFn prologue_fn;
+    Assembler* assembler;
     void* entry_point;
   };
   auto Thread = [](void* void_arg) {
+    SetThreadName("Assembler");
+    printf("Thread started\n");
     ThreadArg* arg = (ThreadArg*)void_arg;
-    prctl(PR_SET_DUMPABLE, 1);
-
-    arg->prologue_fn(arg->entry_point);
+    auto& assembler = *arg->assembler;
+    assembler.is_running = true;
+    auto ret = assembler.prologue_fn(arg->entry_point);
+    assembler.is_running = false;
+    ret -= (uintptr_t)assembler.machine_code.get();
+    printf("Assembler returned: %lx\n", ret);
     return 0;
   };
-  ThreadArg arg = {prologue_fn, entry_point->address};
+  ThreadArg arg = {this, entry_point->address};
+
+  int STACK_SIZE = 8 * 1024 * 1024;
+  std::vector<char> stack;
+  stack.resize(STACK_SIZE);
   if (clone(Thread, (void*)(stack.data() + STACK_SIZE),
             CLONE_PARENT_SETTID | CLONE_SIGHAND | CLONE_FILES | CLONE_FS | CLONE_IO | CLONE_VM,
-            &arg, &v) == -1) {
-    perror("failed to spawn child task");
+            &arg, &pid) == -1) {
+    ERROR << "failed to spawn child task: " << strerror(errno);
     return;
   }
 
-  printf("t1_pid: %d\n", v);
-
-  int ret = ptrace(PTRACE_SEIZE, v, 0, PTRACE_O_TRACESYSGOOD | PTRACE_O_EXITKILL);
-  if (ret != 0) {
-    perror("PTRACE_SEIZE failed");
-    return;
-  }
-
-  LOG << "PTRACE_SEIZE done";
-
-  // Wait for the child to exit
-  waitpid(v, NULL, 0);
-
-  LOG << "Child exited";
+  printf("t1_pid: %d\n", pid);
 #else
   // TODO: Implement on Windows
 #endif  // __linux__
 }
+
+AssemblerWidget::AssemblerWidget(std::weak_ptr<Object> object) {
+  this->object = object;
+  children.emplace_back(std::make_shared<RegisterWidget>(object, 0));
+}
+
+std::string_view AssemblerWidget::Name() const { return "Assembler"; }
+SkPath AssemblerWidget::Shape() const { return SkPath::RRect(kRRect.sk); }
+
+void AssemblerWidget::Draw(SkCanvas& canvas) const {
+  static constexpr float kFlatBorderWidth = 3_mm;
+  static constexpr RRect kBorderMidRRect = kRRect.Outset(-kFlatBorderWidth);
+  static constexpr RRect kInnerRRect = kBorderMidRRect.Outset(-kFlatBorderWidth);
+  float one_pixel = 1.0f / canvas.getTotalMatrix().getScaleX();
+  SkPaint flat_border_paint;
+  flat_border_paint.setColor("#9b252a"_color);
+  canvas.drawDRRect(kRRect.sk, kBorderMidRRect.sk, flat_border_paint);
+  SkPaint bevel_border_paint;
+  bevel_border_paint.setColor("#7d2627"_color);
+  canvas.drawDRRect(kBorderMidRRect.sk, kInnerRRect.sk, bevel_border_paint);
+  SkPaint bg_paint = [&]() {
+    static auto builder =
+        resources::RuntimeEffectBuilder(embedded::assets_assembler_stars_rt_sksl.content);
+
+    builder->uniform("uv_to_pixel") = canvas.getTotalMatrix();
+
+    auto shader = builder->makeShader();
+    SkPaint paint;
+    paint.setShader(shader);
+    return paint;
+  }();
+  canvas.drawRRect(kInnerRRect.Outset(one_pixel).sk, bg_paint);
+
+  Regs regs;
+  GetRegs(object, regs);
+  DrawChildren(canvas);
+}
+
+void AssemblerWidget::FillChildren(maf::Vec<std::shared_ptr<gui::Widget>>& children) {
+  for (auto& child : this->children) {
+    children.push_back(child);
+  }
+}
+
+std::unique_ptr<Action> AssemblerWidget::FindAction(gui::Pointer& p, gui::ActionTrigger btn) {
+  if (btn == gui::PointerButton::Left) {
+    auto* location = Closest<Location>(*p.hover);
+    auto* machine = Closest<Machine>(*p.hover);
+    if (location && machine) {
+      auto contact_point = p.PositionWithin(*this);
+      auto a = std::make_unique<DragLocationAction>(p, machine->Extract(*location));
+      a->contact_point = contact_point;
+      return a;
+    }
+  }
+  return nullptr;
+}
+
+void AssemblerWidget::TransformUpdated() { WakeAnimation(); }
+
+SkPath RegisterWidget::Shape() const { return SkPath::Rect(kBoundingRect.sk); }
+std::string_view RegisterWidget::Name() const { return "Register"; }
+
+static const SkPath kFlagPole = PathFromSVG(
+    "m-.5-.7c-1.8-7.1-2.3-14.5-2.5-21.9-.3.2-.8.3-1.3.4.7-1 1.4-1.8 1.8-3 .3 1.2.8 2 1.6 2.9-.4 "
+    "0-.7-.1-1.2-.3 0 7.4 1 14.7 2.5 21.9.5.2.8.5.9.7h-2.5c.1-.2.3-.5.7-.7z");
+
+static const SkPath kFlag = PathFromSVG(
+    R"(m-3.5-21.7c.2-.5 3.1 1 4.6.9 1.6-.1 3.1-1.4 4.7-1.3 1.5.1 2.6 1.8 4.1 1.9 2 .2 3.9-1.4 6-1.5 2.7-.1 8 1.2 8 1.2s-6.7 1-9.7 2.5c-1.8.8-2.8 3-4.7 3.6-1.3.4-2.6-.7-3.9-.4-1.7.4-2.8 2.2-4.4 2.8-1.3.5-4.1.9-4.2.5-.4-3.4-.8-6.6-.6-10.2z)");
+
+static constexpr float kBitPositionFontSize = RegisterWidget::kCellHeight * 0.42;
+
+static gui::Font& BitPositionFont() {
+  static auto font = gui::Font::MakeV2(gui::Font::GetGrenzeRegular(), kBitPositionFontSize);
+  return *font;
+}
+
+static constexpr float kByteValueFontSize = 3_mm;  // RegisterWidget::kCellHeight * 1;
+
+static gui::Font& ByteValueFont() {
+  static auto font = gui::Font::MakeV2(gui::Font::GetHeavyData(), kByteValueFontSize);
+  return *font;
+}
+
+// Shift the byte values up so that they're vertically centered with their rows
+static constexpr float kByteValueFontShiftUp =
+    (RegisterWidget::kCellHeight - kByteValueFontSize) / 2;
+
+// Shift the font up, so that its top is aligned with the middle of the cell
+static constexpr float kBitPositionFontShiftUp =
+    RegisterWidget::kCellHeight / 2 - kBitPositionFontSize;
+
+void RegisterWidget::Draw(SkCanvas& canvas) const {
+  auto object_shared = object.lock();
+  auto assembler = static_cast<Assembler*>(object_shared.get());
+  uint64_t reg_value = (*assembler->regs)[register_index];
+
+  SkPaint dark_paint;
+  dark_paint.setColor("#dcca85"_color);
+  canvas.drawRect(kBaseRect.sk, dark_paint);
+  SkPaint light_paint;
+  light_paint.setColor("#fefdfb"_color);
+
+  auto& bit_position_font = BitPositionFont();
+  auto& byte_value_font = ByteValueFont();
+  for (int row = 0; row < 8; ++row) {
+    float bottom = kInnerRect.bottom + kCellHeight * row;
+    float top = bottom + kCellHeight;
+    int byte_value = (reg_value >> (row * 8)) & 0xFF;
+    canvas.save();
+    canvas.translate(kBaseRect.right + 0.5_mm, bottom + kByteValueFontShiftUp);
+    auto byte_value_str = f("%X", byte_value);
+    byte_value_font.DrawText(canvas, byte_value_str, dark_paint);
+    canvas.restore();
+    for (int bit = 0; bit < 8; ++bit) {
+      float right = kInnerRect.right - kCellWidth * bit;
+      float left = right - kCellWidth;
+      SkPaint* cell_paint = &light_paint;
+      if (bit % 2 == row % 2) {
+        // light cell
+        canvas.drawRect(SkRect::MakeLTRB(left, bottom, right, top), light_paint);
+        cell_paint = &dark_paint;
+      }
+
+      int position = row * 8 + bit;
+      std::string position_str = f("%d", position);
+      float position_text_width = bit_position_font.MeasureText(position_str);
+      canvas.save();
+      canvas.translate(left + (kCellWidth - position_text_width) * 0.5,
+                       bottom + kBitPositionFontShiftUp);
+      bit_position_font.DrawText(canvas, position_str, *cell_paint);
+      canvas.restore();
+
+      SkPaint pole_paint;
+      SkPaint flag_paint;
+      SkPoint points[2] = {SkPoint::Make(-kCellWidth * 0.2, 0),
+                           SkPoint::Make(kCellWidth * 1.2, kCellHeight * 0.1)};
+      SkColor colors[5] = {"#ff0000"_color, "#800000"_color, "#ff0000"_color, "#800000"_color,
+                           "#ff0000"_color};
+      flag_paint.setShader(
+          SkGradientShader::MakeLinear(points, colors, nullptr, 5, SkTileMode::kClamp));
+      if (reg_value & (1ULL << position)) {
+        canvas.save();
+        canvas.translate(left + kCellWidth * 0.2, bottom);
+        canvas.scale(0.5, 0.5);
+        canvas.drawPath(kFlagPole, pole_paint);
+        canvas.drawPath(kFlag, flag_paint);
+        canvas.restore();
+      }
+    }
+  }
+
+  canvas.save();
+
+  canvas.translate(-kRegisterIconWidth / 2, kBaseRect.top - kRegisterIconWidth * 0.15);
+  kRegisters[register_index].image.draw(canvas);
+  canvas.restore();
+}
+
 }  // namespace automat::library
--- src/library_assembler.hh
+++ src/library_assembler.hh
@@ -22,19 +22,63 @@ struct DeleteWithMunmap {
 
 struct Regs;
 
+struct RegisterWidget : gui::Widget {
+  std::weak_ptr<Object> object;  // assembler
+  int register_index;
+  constexpr static Rect kBaseRect = Rect::MakeAtZero<CenterX, CenterY>(3_cm, 3_cm);
+  constexpr static Rect kBoundingRect = []() {
+    auto rect = kBaseRect;
+    rect.top += 11_mm;   // space for the register icon
+    rect.right += 1_cm;  // space for byte values
+    return rect;
+  }();
+  constexpr static Rect kInnerRect = kBaseRect.Outset(-1_mm);
+  constexpr static float kCellHeight = kInnerRect.Height() / 8;
+  constexpr static float kCellWidth = kInnerRect.Width() / 8;
+
+  RegisterWidget(std::weak_ptr<Object> object, int register_index)
+      : object(object), register_index(register_index) {}
+  std::string_view Name() const override;
+  SkPath Shape() const override;
+  void Draw(SkCanvas&) const override;
+};
+
+struct AssemblerWidget : gui::Widget {
+  constexpr static float kWidth = 8_cm;
+  constexpr static float kHeight = 8_cm;
+  constexpr static float kRadius = 1_cm;
+  constexpr static RRect kRRect =
+      RRect::MakeSimple(Rect::MakeAtZero<CenterX, CenterY>(kWidth, kHeight), kRadius);
+
+  std::weak_ptr<Object> object;
+  maf::Vec<std::shared_ptr<gui::Widget>> children;
+
+  AssemblerWidget(std::weak_ptr<Object> object);
+  std::string_view Name() const override;
+  void FillChildren(maf::Vec<std::shared_ptr<gui::Widget>>& children) override;
+  SkPath Shape() const override;
+  void Draw(SkCanvas&) const override;
+  std::unique_ptr<Action> FindAction(gui::Pointer& p, gui::ActionTrigger btn) override;
+  void TransformUpdated() override;
+};
+
 // Combines functions of Assembler and Thread.
 // Assembler part takes care of emitting machine code to an executable memory region.
 // Thread part maintains register state across executions.
 struct Assembler : LiveObject {
-  std::unique_ptr<char, DeleteWithMunmap> machine_code;
-
-  std::unique_ptr<Regs> regs;
-
-  std::shared_ptr<Object> Clone() const override;
-
   using PrologueFn = uintptr_t (*)(void*);
 
+  std::unique_ptr<char, DeleteWithMunmap> machine_code;
+  std::unique_ptr<Regs> regs;
   PrologueFn prologue_fn = nullptr;
+#if defined __linux__
+
+  std::atomic<bool> is_running = false;
+  pid_t pid = 0;  // int actually
+
+#endif  // __linux__
+
+  std::shared_ptr<Object> Clone() const override;
 
   Assembler(maf::Status&);
   ~Assembler();
@@ -43,13 +87,9 @@ struct Assembler : LiveObject {
 
   void RunMachineCode(library::Instruction* entry_point);
 
-  struct Widget : FallbackWidget {
-    Widget(std::weak_ptr<Object> object) : FallbackWidget() { this->object = object; }
-    float Width() const override { return 6_cm; }
-    std::string Text() const override;
-  };
-
-  std::shared_ptr<gui::Widget> MakeWidget() override { return std::make_shared<Widget>(WeakPtr()); }
+  std::shared_ptr<gui::Widget> MakeWidget() override {
+    return std::make_shared<AssemblerWidget>(WeakPtr());
+  }
 };
 
 }  // namespace automat::library

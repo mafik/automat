c35a47409624fdc6ed79866f1bae6ba19861a256
Marek Rogalski <mafikpl@gmail.com>
2025-03-12 12:40:24 +0100

    Generalize rrect border shader

--- src/library_assembler.cc
+++ src/library_assembler.cc
@@ -10,6 +10,7 @@
 #include <llvm/lib/Target/X86/X86Subtarget.h>
 
 #include "animation.hh"
+#include "drawing.hh"
 #include "embedded.hh"
 #include "font.hh"
 #include "global_resources.hh"
@@ -299,38 +300,6 @@ animation::Phase AssemblerWidget::Tick(time::Timer& timer) {
   }
 
   return phase;
-}
-
-void SetRRectShader(SkPaint& paint, const RRect& rrect, SkColor top, SkColor middle,
-                    SkColor bottom) {
-  // Get the center point of the rounded rectangle
-  SkPoint center = rrect.Center();
-
-  // Define color stops for a sweep gradient
-  // We'll use strategic positions to create the transitions between colors
-  constexpr int count = 8;
-  SkColor colors[count] = {
-      middle,  // right top
-      top,     // top right
-      top,     // top left
-      middle,  // left top
-      middle,  // left bottom
-      bottom,  // bottom left
-      bottom,  // bottom right
-      middle,  // right bottom
-  };
-
-  auto Angle = [](Vec2 v) -> float { return SinCos::FromVec2(v).ToRadiansPositive() / M_PI / 2; };
-
-  // Position stops at strategic angles (in 0-1 range where 1.0 = 360°, 0 = stright right)
-  float positions[count] = {
-      Angle(rrect.LineEndRightUpper()), Angle(rrect.LineEndUpperRight()),
-      Angle(rrect.LineEndUpperLeft()),  Angle(rrect.LineEndLeftUpper()),
-      Angle(rrect.LineEndLeftLower()),  Angle(rrect.LineEndLowerLeft()),
-      Angle(rrect.LineEndLowerRight()), Angle(rrect.LineEndRightLower()),
-  };
-
-  paint.setShader(SkGradientShader::MakeSweep(center.x(), center.y(), colors, positions, count));
 }
 
 void AssemblerWidget::Draw(SkCanvas& canvas) const {

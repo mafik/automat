d0f6db3d3ce8cb3a1a60a22d5232b15b0234cd01
Marek Rogalski <mafikpl@gmail.com>
2025-03-12 13:07:41 +0100

    Window Widget

--- src/library_window.cc
+++ src/library_window.cc
@@ -18,8 +18,21 @@ std::string_view Window::Name() const { return "Window"; }
 
 std::shared_ptr<Object> Window::Clone() const { return std::make_shared<Window>(*this); }
 
+const SkMatrix kCenterPickIcon = SkMatrix::Translate(-1_mm, 0);
+
+constexpr static float kBorderWidth = 0.5_mm;    // width of the border
+constexpr static float kContentMargin = 0.5_mm;  // margin between the border and the content
+constexpr static float kTitleHeight = 8_mm;      // height of the title bar
+constexpr static float kTitleButtonSize = kTitleHeight - 2 * kContentMargin;
+
 struct PickButton : gui::Button {
-  PickButton() : gui::Button(gui::MakeShapeWidget(kPickSVG, "#000000"_color)) {}
+  PickButton() : gui::Button(gui::MakeShapeWidget(kPickSVG, "#000000"_color, &kCenterPickIcon)) {}
+  SkRRect RRect() const override {
+    return RRect::MakeSimple(
+               Rect::MakeAtZero<::LeftX, ::BottomY>({kTitleButtonSize, kTitleButtonSize}), 1_mm)
+        .sk;
+  }
+  SkColor BackgroundColor() const override { return "#d0d0d0"_color; }
 };
 
 struct WindowWidget : gui::Widget {
@@ -35,6 +48,14 @@ struct WindowWidget : gui::Widget {
 
   WindowWidget(std::weak_ptr<Window>&& window) : window(std::move(window)) {
     pick_button = std::make_shared<PickButton>();
+    auto content_bounds = CoarseBounds().Outset(-kBorderWidth - kContentMargin);
+    auto title_bounds = Rect(content_bounds.rect.left, content_bounds.rect.top - kTitleHeight,
+                             content_bounds.rect.right, content_bounds.rect.top);
+
+    auto pos = title_bounds.RightCenter();
+    pos.x -= kTitleButtonSize + kContentMargin;
+    pos.y -= kTitleButtonSize / 2;
+    pick_button->local_to_parent = SkM44::Translate(pos.x, pos.y);
   }
 
   RRect CoarseBounds() const override {
@@ -45,7 +66,7 @@ struct WindowWidget : gui::Widget {
 
   void Draw(SkCanvas& canvas) const override {
     auto outer_rrect = CoarseBounds();
-    auto border_inner = outer_rrect.Outset(-0.5_mm);
+    auto border_inner = outer_rrect.Outset(-kBorderWidth);
     SkPaint inner_paint;
     inner_paint.setColor("#c0c0c0"_color);
     canvas.drawRRect(border_inner.sk, inner_paint);
@@ -53,8 +74,8 @@ struct WindowWidget : gui::Widget {
     SetRRectShader(border_paint, border_inner, "#e7e5e2"_color, "#9b9b9b"_color, "#3b3b3b"_color);
     canvas.drawDRRect(outer_rrect.sk, border_inner.sk, border_paint);
 
-    auto contents_rrect = border_inner.Outset(-0.5_mm);
-    Rect title_rect = Rect(contents_rrect.rect.left, contents_rrect.rect.top - 8_mm,
+    auto contents_rrect = border_inner.Outset(-kContentMargin);
+    Rect title_rect = Rect(contents_rrect.rect.left, contents_rrect.rect.top - kTitleHeight,
                            contents_rrect.rect.right, contents_rrect.rect.top);
     SkPaint title_paint;
     SkColor title_colors[] = {"#0654cb"_color, "#030058"_color};

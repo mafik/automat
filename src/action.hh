afaad7c8c8fe04af52be7b635abac75ff5ccfb80
Marek Rogalski <mafikpl@gmail.com>
2025-03-24 14:39:45 +0100

    Document Action

--- src/action.hh
+++ src/action.hh
@@ -15,23 +15,38 @@ struct Widget;
 
 struct Action;
 
+// Option represents a potential action. It's the core of the menu system.
 struct Option {
   virtual ~Option() = default;
   virtual maf::StrView Name() const = 0;
   virtual std::unique_ptr<Action> Activate(gui::Pointer& pointer) const = 0;
 };
 
+// Action represents an action/gesture that user can perform by pressing a key / button / touching
+// the screen and then moving the pointer around before releasing it.
+//
+// Actions are the main mechanism for user to interact with the UI.
 struct Action {
   gui::Pointer& pointer;
+
+  // Each action must be bound to a pointer. A reference to the pointer is stored internally to keep
+  // track of its position.
   Action(gui::Pointer& pointer) : pointer(pointer) {}
+
+  // Action is destroyed when the pointer is released. This typically corresponds to the button
+  // release or key up.
   virtual ~Action() = default;
+
+  // Update is called when the pointer moves (although spurious calls are also possible). This
+  // function may be called hundreds of times per second.
   virtual void Update() = 0;
+
   virtual gui::Widget* Widget() { return nullptr; }
 };
 
 /* Keeping this around for copy-pasting
-struct MinimalActionTemplate : Action {
-  MinimalActionTemplate(gui::Pointer& pointer) : Action(pointer) {}
+struct ActionTemplate : Action {
+  ActionTemplate(gui::Pointer& pointer) : Action(pointer) {}
   void Update() override {}
 };
 */

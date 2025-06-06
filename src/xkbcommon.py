16f066259d2c80172f39b61779e2f6488b081e13
Marek Rogalski <mafikpl@gmail.com>
2025-06-06 16:56:36 +0200

    Make xkbcommon depend on libxcb

--- src/xkbcommon.py
+++ src/xkbcommon.py
@@ -7,10 +7,12 @@ import extension_helper
 import src
 
 if sys.platform == 'linux':
+  xcb = src.load_extension('xcb')
   hook = extension_helper.ExtensionHelper('xkbcommon', globals())
   hook.FetchFromURL('https://github.com/xkbcommon/libxkbcommon/archive/refs/tags/xkbcommon-1.8.1.tar.gz')
   hook.ConfigureWithMeson('{PREFIX}/lib64/libxkbcommon.a')
   hook.ConfigureOption('enable-x11', 'true')
   hook.ConfigureOption('enable-tools', 'false')
+  hook.ConfigureDependsOn(xcb.libxcb)
   hook.AddLinkArgs('-l:libxkbcommon.a', '-l:libxkbcommon-x11.a', '-l:libxcb-xkb.a')
   hook.InstallWhenIncluded(r'^xkbcommon/.*\.h$')

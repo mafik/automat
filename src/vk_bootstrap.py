7cf929f1dfb1395c7a51e0ea0659e4d535ddb4fd
Marek Rogalski <mafikpl@gmail.com>
2025-06-09 16:26:07 +0200

    Fix duplicated vulkan dependency

--- src/vk_bootstrap.py
+++ src/vk_bootstrap.py
@@ -7,6 +7,7 @@ libname = build.libname('vk-bootstrap')
 vulkan_headers = extension_helper.ExtensionHelper('vulkan-headers', globals())
 vulkan_headers.FetchFromGit('https://github.com/KhronosGroup/Vulkan-Headers', 'vulkan-sdk-1.4.313.0')
 vulkan_headers.ConfigureWithCMake(build.PREFIX / 'include' / 'vulkan' / 'vulkan.h')
+vulkan_headers.InstallWhenIncluded(r'^vulkan/vulkan\.h$')
 
 vk_bootstrap = extension_helper.ExtensionHelper('vk-bootstrap', globals())
 vk_bootstrap.FetchFromGit('https://github.com/charles-lunarg/vk-bootstrap', 'v1.3.290')

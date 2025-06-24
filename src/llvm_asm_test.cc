b504a642a442ce4393c0aaa6f502085b1483741a
Marek Rogalski <mafikpl@gmail.com>
2025-06-24 12:06:59 +0200

    Fix LLVM Asm test

--- src/llvm_asm_test.cc
+++ src/llvm_asm_test.cc
@@ -19,19 +19,20 @@
 #include "status.hh"
 
 using namespace std::chrono_literals;
+using namespace automat;
 
 class MachineCodeControllerTest : public ::testing::Test {
  protected:
   void SetUp() override {
-    root = MakePtr<automat::Machine>();
-    auto ExitCallback = [this](automat::mc::CodePoint code_point) {
+    root = MakePtr<Machine>();
+    auto ExitCallback = [this](mc::CodePoint code_point) {
       std::unique_lock<std::mutex> lock(mutex);
       exit_instr = *code_point.instruction;  // copy the weak_ptr
       exit_point = code_point.stop_type;
       exited = true;
       cv.notify_all();
     };
-    controller = automat::mc::Controller::Make(ExitCallback);
+    controller = mc::Controller::Make(ExitCallback);
   }
 
   void TearDown() override {
@@ -39,12 +40,12 @@ class MachineCodeControllerTest : public ::testing::Test {
     controller.reset();
   }
 
-  void StartExecution(WeakPtr<automat::library::Instruction> instr) {
+  void StartExecution(WeakPtr<library::Instruction> instr) {
     exited = false;
     exit_instr = {};
-    exit_point = automat::mc::StopType::InstructionBody;
+    exit_point = mc::StopType::InstructionBody;
     Status status;
-    WeakPtr<automat::mc::Inst> mc_instr = instr.lock()->ToMC();
+    NestedWeakPtr<const mc::Inst> mc_instr = instr.lock()->ToMC();
     controller->Execute(mc_instr, status);
     ASSERT_TRUE(OK(status));
   }
@@ -59,19 +60,19 @@ class MachineCodeControllerTest : public ::testing::Test {
     return true;
   }
 
-  void ExpectWeakPtrsEqual(WeakPtr<const automat::mc::Inst> expected,
-                           WeakPtr<const automat::mc::Inst> actual, const std::string& name) {
-    auto expected_shared = expected.lock();
-    auto actual_shared = actual.lock();
-    auto expected_ptr = expected_shared.get();
-    auto actual_ptr = actual_shared.get();
+  void ExpectWeakPtrsEqual(NestedWeakPtr<const mc::Inst> expected,
+                           NestedWeakPtr<const mc::Inst> actual, const std::string& name) {
+    auto expected_shared = expected.Lock();
+    auto actual_shared = actual.Lock();
+    auto expected_ptr = expected_shared.Get();
+    auto actual_ptr = actual_shared.Get();
     EXPECT_EQ(expected_ptr, actual_ptr) << name << " ptrs not equal";
   }
 
-  void VerifyState(
-      automat::mc::Controller::State expected, WeakPtr<automat::mc::Inst> expected_exit_instr = {},
-      automat::mc::StopType expected_exit_point = automat::mc::StopType::InstructionBody) {
-    automat::mc::Controller::State state;
+  void VerifyState(mc::Controller::State expected,
+                   NestedWeakPtr<const mc::Inst> expected_exit_instr = {},
+                   mc::StopType expected_exit_point = mc::StopType::InstructionBody) {
+    mc::Controller::State state;
     Status status;
     controller->GetState(state, status);
     ExpectWeakPtrsEqual(expected.current_instruction, state.current_instruction,
@@ -95,46 +96,44 @@ class MachineCodeControllerTest : public ::testing::Test {
     EXPECT_EQ(exit_point, expected_exit_point);
   }
 
-  Ptr<automat::library::Instruction> MakeInstructionRegImm(unsigned opcode, unsigned reg,
-                                                           int64_t imm) {
+  Ptr<library::Instruction> MakeInstructionRegImm(unsigned opcode, unsigned reg, int64_t imm) {
     return MakeInstruction(llvm::MCInstBuilder(opcode).addReg(reg).addImm(imm));
   }
 
-  Ptr<automat::library::Instruction> MakeInstructionImm(unsigned opcode, int64_t imm) {
+  Ptr<library::Instruction> MakeInstructionImm(unsigned opcode, int64_t imm) {
     return MakeInstruction(llvm::MCInstBuilder(opcode).addImm(imm));
   }
 
-  Ptr<automat::library::Instruction> MakeInstruction(const llvm::MCInst& mc_inst) {
+  Ptr<library::Instruction> MakeInstruction(const llvm::MCInst& mc_inst) {
     auto& loc = root->CreateEmpty();
-    auto inst = MakePtr<automat::library::Instruction>();
+    auto inst = MakePtr<library::Instruction>();
     inst->mc_inst = mc_inst;
     inst->mc_inst.setLoc(llvm::SMLoc::getFromPointer((const char*)inst.get()));
-    loc.InsertHereNoWidget(Ptr<automat::library::Instruction>(inst));  // copy Ptr
+    loc.InsertHereNoWidget(Ptr<library::Instruction>(inst));  // copy Ptr
     return inst;
   }
 
-  automat::Connection* Next(Ptr<automat::library::Instruction>& a,
-                            Ptr<automat::library::Instruction>& b) {
-    return a->here.lock()->ConnectTo(*b->here.lock(), automat::next_arg);
+  Connection* Next(Ptr<library::Instruction>& a, Ptr<library::Instruction>& b) {
+    return a->here.lock()->ConnectTo(*b->here.lock(), next_arg);
   }
 
-  void TestUpdateCode(std::span<automat::library::Instruction*> instructions) {
-    std::vector<Ptr<automat::library::Instruction>> instructions_objs;
+  void TestUpdateCode(std::span<library::Instruction*> instructions) {
+    std::vector<Ptr<library::Instruction>> instructions_objs;
     for (auto* inst : instructions) {
-      instructions_objs.push_back(inst->SharedPtr());
+      instructions_objs.push_back(inst->AcquirePtr());
     }
     Status status;
     UpdateCode(*controller, std::move(instructions_objs), status);
     ASSERT_TRUE(OK(status)) << status.ToStr();
   }
 
-  std::unique_ptr<automat::mc::Controller> controller;
+  std::unique_ptr<mc::Controller> controller;
   std::mutex mutex;
   std::condition_variable cv;
   bool exited = false;
-  WeakPtr<const automat::mc::Inst> exit_instr = {};
-  automat::mc::StopType exit_point = automat::mc::StopType::InstructionBody;
-  Ptr<automat::Machine> root;
+  NestedWeakPtr<const mc::Inst> exit_instr = {};
+  mc::StopType exit_point = mc::StopType::InstructionBody;
+  Ptr<Machine> root;
 };
 
 TEST_F(MachineCodeControllerTest, InitialState) { VerifyState({.regs = {.RAX = 0}}); }
@@ -142,28 +141,28 @@ TEST_F(MachineCodeControllerTest, InitialState) { VerifyState({.regs = {.RAX = 0
 // Checks that a single instruction can be executed correctly
 TEST_F(MachineCodeControllerTest, SingleInstruction) {
   auto inst = MakeInstructionRegImm(llvm::X86::MOV64ri, llvm::X86::RAX, 1337);
-  automat::library::Instruction* instructions[] = {inst.get()};
+  library::Instruction* instructions[] = {inst.get()};
   TestUpdateCode(instructions);
 
   StartExecution(inst);
   ASSERT_TRUE(WaitForExecution());
-  VerifyState({.regs = {.RAX = 1337}}, inst->ToMC(), automat::mc::StopType::Next);
+  VerifyState({.regs = {.RAX = 1337}}, inst->ToMC(), mc::StopType::Next);
 }
 
 // Two separate instructions, executed one at a time
 TEST_F(MachineCodeControllerTest, TwoSeparateInstructions) {
   auto inst1 = MakeInstructionRegImm(llvm::X86::MOV64ri, llvm::X86::RAX, 1337);
   auto inst2 = MakeInstructionRegImm(llvm::X86::MOV64ri, llvm::X86::RBX, 42);
-  automat::library::Instruction* instructions[] = {inst1.get(), inst2.get()};
+  library::Instruction* instructions[] = {inst1.get(), inst2.get()};
   TestUpdateCode(instructions);
 
   StartExecution(inst2);
   ASSERT_TRUE(WaitForExecution());
-  VerifyState({.regs = {.RBX = 42}}, inst2->ToMC(), automat::mc::StopType::Next);
+  VerifyState({.regs = {.RBX = 42}}, inst2->ToMC(), mc::StopType::Next);
 
   StartExecution(inst1);
   ASSERT_TRUE(WaitForExecution());
-  VerifyState({.regs = {.RAX = 1337, .RBX = 42}}, inst1->ToMC(), automat::mc::StopType::Next);
+  VerifyState({.regs = {.RAX = 1337, .RBX = 42}}, inst1->ToMC(), mc::StopType::Next);
 }
 
 // Two instructions, executed one after the other
@@ -171,12 +170,12 @@ TEST_F(MachineCodeControllerTest, TwoSequentialInstructions) {
   auto inst1 = MakeInstructionRegImm(llvm::X86::MOV64ri, llvm::X86::RAX, 1337);
   auto inst2 = MakeInstructionRegImm(llvm::X86::MOV64ri, llvm::X86::RBX, 42);
   Next(inst1, inst2);
-  automat::library::Instruction* instructions[] = {inst1.get(), inst2.get()};
+  library::Instruction* instructions[] = {inst1.get(), inst2.get()};
   TestUpdateCode(instructions);
 
   StartExecution(inst1);
   ASSERT_TRUE(WaitForExecution());
-  VerifyState({.regs = {.RAX = 1337, .RBX = 42}}, inst2->ToMC(), automat::mc::StopType::Next);
+  VerifyState({.regs = {.RAX = 1337, .RBX = 42}}, inst2->ToMC(), mc::StopType::Next);
 }
 
 TEST_F(MachineCodeControllerTest, JumpExitInstruction) {
@@ -184,20 +183,20 @@ TEST_F(MachineCodeControllerTest, JumpExitInstruction) {
   // This should trigger a fixup that generates an exit at a Jump exit point.
   auto inst = MakeInstructionImm(llvm::X86::JMP_4, 0);
 
-  automat::library::Instruction* instructions[] = {inst.get()};
+  library::Instruction* instructions[] = {inst.get()};
   TestUpdateCode(instructions);
 
   StartExecution(inst);
   ASSERT_TRUE(WaitForExecution());
 
   // Expect that registers remain zero and that we exit at a Jump exit point.
-  VerifyState({.regs = {}}, inst->ToMC(), automat::mc::StopType::Jump);
+  VerifyState({.regs = {}}, inst->ToMC(), mc::StopType::Jump);
 }
 
 TEST_F(MachineCodeControllerTest, InfiniteLoop) {
   auto inst = MakeInstructionRegImm(llvm::X86::MOV64ri, llvm::X86::RAX, 42);
   Next(inst, inst);
-  automat::library::Instruction* instructions[] = {inst.get()};
+  library::Instruction* instructions[] = {inst.get()};
   TestUpdateCode(instructions);
   StartExecution(inst);
   ASSERT_FALSE(WaitForExecution(10ms));
@@ -211,7 +210,7 @@ TEST_F(MachineCodeControllerTest, HotReload) {
 
   // Start executing inst2 in a loop.
   auto conn = Next(inst2, inst2);
-  automat::library::Instruction* instructions[] = {inst1.get(), inst2.get()};
+  library::Instruction* instructions[] = {inst1.get(), inst2.get()};
   TestUpdateCode(instructions);
   StartExecution(inst2);
   ASSERT_FALSE(WaitForExecution(10ms));
@@ -220,8 +219,8 @@ TEST_F(MachineCodeControllerTest, HotReload) {
   // Then break the loop by redirecting inst2 to inst1.
   delete conn;
   Next(inst2, inst1);
-  automat::library::Instruction* instructions2[] = {inst2.get(), inst1.get()};
+  library::Instruction* instructions2[] = {inst2.get(), inst1.get()};
   TestUpdateCode(instructions2);
   ASSERT_TRUE(WaitForExecution());
-  VerifyState({.regs = {.RAX = 1337}}, inst1->ToMC(), automat::mc::StopType::Next);
+  VerifyState({.regs = {.RAX = 1337}}, inst1->ToMC(), mc::StopType::Next);
 }

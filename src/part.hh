cd4960f33db6fbd247444895c26abbc258051b2e
Marek Rogalski <mafikpl@gmail.com>
2026-01-14 11:12:44 +0100

    Some docs on parts

--- src/part.hh
+++ src/part.hh
@@ -9,10 +9,48 @@
 
 namespace automat {
 
-// The base type for all things that can be named.
+// Part is a virtual(*) base class for most things in Automat.
 //
-// Due to possibilty of this class present multiple times in the inheritance (diamond inheritance),
-// this should probably be inherited virtually.
+// (*) "virtual" - means that even if some object has a complex, diamond-type inheritance, it's
+// always going to have exactly one `Part` base.
+//
+// Although Part itself doesn't expose much methods (only the ability to identify the specific
+// type), it's role is that it's a base for many interfaces.
+//
+// # Purpose
+//
+// TL;DR is:
+//
+// 1. Parts allow Objects to act in a *generic* way.
+// 2. Parts allow basic code reuse across Objects.
+//
+// Parts primary role is to allow objects to behave in a generic fashion. Parts are like programming
+// interfaces that expose different behaviors in a standardized way.
+//
+// Objects expose their parts using Object::Parts function. Automat infrastructure uses this to
+// automatically populate menu with various options, help with (de)serialization of state, visualize
+// connections between parts etc.
+//
+// # Notable subclasses
+//
+// - Argument (argument.hh/cc) - allows objects to link to (parts of) other objects
+// - Interface (interface.hh/cc) - allows objects to synchronize their behavior
+//
+// # Representation
+//
+// TODO: document:
+// 1. memory representation (weak ptr, external parts)
+// 2. menu system / serialization
+// 3. visual representation
+//
+// Parts are identified by their memory addresses. However, due to concurrent nature of Automat,
+// where objects can go away at any time.
+//
+// # TODOs
+//
+// TODO: rename & cleanup of Synchronization.md / interface.hh/cc
+// TODO: design & document the approach to serialization
+// TODO: design & document menu generation
 struct Part {
   virtual ~Part() = default;
 

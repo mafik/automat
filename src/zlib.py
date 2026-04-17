caf0b38e749e783739a838a4e4bc9fe649399746
Marek Rogalski <mafikpl@gmail.com>
2026-04-17 14:17:56 +0200

    Make zlib position-independent

--- src/zlib.py
+++ src/zlib.py
@@ -9,6 +9,7 @@ hook = extension_helper.ExtensionHelper('zlib', globals())
 hook.FetchFromGit('https://github.com/madler/zlib.git', 'v1.3.1')
 hook.ConfigureWithCMake(build.PREFIX / 'include' / 'zlib.h')
 hook.ConfigureOption('BUILD_SHARED_LIBS', 'OFF')
+hook.ConfigureOption('CMAKE_POSITION_INDEPENDENT_CODE', 'ON')
 if build.platform == 'win32':
   hook.AddLinkArg('-lzlibstatic')
 else:

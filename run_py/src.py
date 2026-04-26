b98ced9aa0b294b117d2328836cf6d97669b30d9
Marek Rogalski <mafikpl@gmail.com>
2026-04-26 12:21:45 +0200

    Better embedding (sharding, #embed-tracking)

--- run_py/src.py
+++ run_py/src.py
@@ -18,6 +18,7 @@ class File:
     comment_libs: list[str]
     direct_includes: list[str] # absolute paths as strings
     transitive_includes: set['File']
+    embeds: list[str]
     link_args: dict[str, list[str]]
     compile_args: dict[str, list[str]]
     run_args: dict[str, list[str]]
@@ -28,18 +29,19 @@ class File:
         self.system_includes = []
         self.comment_libs = []
         self.direct_includes = []
+        self.transitive_includes = set()
+        self.embeds = []
         self.link_args = defaultdict(list)
         self.compile_args = defaultdict(list)
         self.run_args = defaultdict(list)
         self.main = False
         self.manifest = False
-        self.transitive_includes = set()
 
     def is_header(self) -> bool:
-        return self.path.suffix in ('.h', '.hh')
+        return self.path.suffix in ('.h', '.hh', '.hpp')
 
     def is_source(self) -> bool:
-        return self.path.suffix in ('.c', '.cc')
+        return self.path.suffix in ('.c', '.cc', '.cpp')
 
     def build_link_args(self, build_type: str) -> list[str]:
         return self.link_args.get(build_type, []) + (self.link_args.get('', []) if build_type else [])
@@ -129,6 +131,11 @@ class File:
             match = re.match(r'^#pragma maf manifest', line)
             if match:
                 self.manifest = True
+
+            # TODO: handle <> & escapes
+            match = re.match(r'^#embed "(.*)"', line)
+            if match:
+                self.embeds.append(match.group(1))
 
     # This should be called after all files are scanned
     def update_transitive_includes(self, srcs: dict[str, 'File']):

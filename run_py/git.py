052676da695705c8efc070e0b19e188b67fbc801
Marek Rogalski <mafikpl@gmail.com>
2026-04-27 13:39:31 +0200

    Fix for git-on-windows protecting some files

--- run_py/git.py
+++ run_py/git.py
@@ -2,7 +2,9 @@
 # SPDX-FileCopyrightText: Copyright 2024 Automat Authors
 # SPDX-License-Identifier: MIT
 
+import os
 import shutil
+import stat
 from pathlib import Path
 
 import make
@@ -10,7 +12,8 @@ import make
 def clone(url, out_directory, tag):
   def runner():
     if Path(out_directory).exists():
-      shutil.rmtree(out_directory)
+      # Note: git marks some files as read-only, so we need to make them writable before we can delete them.
+      shutil.rmtree(out_directory, onexc=lambda f, p, _: (os.chmod(p, stat.S_IWRITE), f(p)))
     return make.Popen([
       'git',
       '-c', 'advice.detachedHead=false',

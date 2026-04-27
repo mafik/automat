780f6238ba59f75056e7995027cf4b695c75557d
Marek Rogalski <mafikpl@gmail.com>
2026-04-27 16:34:44 +0200

    Less trigger-happy deletion of third_party dependencies

--- run_py/git.py
+++ run_py/git.py
@@ -6,21 +6,48 @@ import os
 import shutil
 import stat
 from pathlib import Path
+from functools import partial
 
-import make
+# Safe (& idempotent) wrapper around git clone.
+# Creates a tombstone before running the command.
+# If a tombstone already exists, removes the git dir before cloning.
+if __name__ == '__main__':
+  import sys
+  import subprocess
 
-def clone(url, out_directory, tag):
-  def runner():
-    if Path(out_directory).exists():
-      # Note: git marks some files as read-only, so we need to make them writable before we can delete them.
-      shutil.rmtree(out_directory, onexc=lambda f, p, _: (os.chmod(p, stat.S_IWRITE), f(p)))
-    return make.Popen([
-      'git',
+  tag, url, out_directory = sys.argv[1:] # layout defined by clone() below
+  out_directory = Path(out_directory)
+
+  # Tombstone indicates that something went wrong during last clone attempt.
+  sentinel = out_directory.with_suffix('.cloning_sentinel')
+  if sentinel.exists():
+    # Something went wrong during last attempt!
+    # Nuke the old checkout - will force a fresh clone.
+    print(f'Tombstone found at {sentinel}, removing {out_directory} before cloning.')
+    # Note: git marks some files as read-only, so we need to make them writable before we can delete them.
+    shutil.rmtree(out_directory, onexc=lambda f, p, _: (os.chmod(p, stat.S_IWRITE), f(p)))
+  else:
+    # Create the tombstone before attempting any risky operations.
+    sentinel.touch()
+
+  if out_directory.exists():
+    subprocess.run(['git',
+      '-c', 'advice.detachedHead=false',
+      '-c', 'core.autocrlf=false',
+      'fetch', '--depth', '1', url, tag], cwd=out_directory, check=True)
+    subprocess.run(['git', 'reset', '--hard', 'FETCH_HEAD'], cwd=out_directory, check=True)
+    os.utime(out_directory, None)  # bump mtime so make.py sees the update
+  else:
+    subprocess.run(['git',
       '-c', 'advice.detachedHead=false',
       '-c', 'core.autocrlf=false',
       'clone',
-      '--depth', '1',
-      '--branch', tag,
-      url, out_directory])
+      '--depth', '1', '--branch', tag, url, str(out_directory)], check=True)
+  
+  sentinel.unlink()
 
-  return runner
+else: # for importing as a module
+  import make
+
+  def clone(url, out_directory, tag):
+    return partial(make.Popen, ['python', __file__, tag, url, out_directory])

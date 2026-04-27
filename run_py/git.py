9d4e4666a395fec4c4062c1febed8c0fde910ddc
Marek Rogalski <mafikpl@gmail.com>
2026-04-27 13:15:18 +0200

    Be more aggressive with stale third_party deps

--- run_py/git.py
+++ run_py/git.py
@@ -8,16 +8,8 @@ from pathlib import Path
 import make
 
 def clone(url, out_directory, tag):
-  out_directory = Path(out_directory)
-  git_opts = '-c advice.detachedHead=false -c core.autocrlf=false'
-
   def runner():
-    if (out_directory / '.git').is_dir():
-      return make.Popen([
-        f'git {git_opts} -C {out_directory} fetch --force --depth 1 origin {tag} && '
-        f'git {git_opts} -C {out_directory} reset --hard FETCH_HEAD'],
-        shell=True)
-    if out_directory.exists():
+    if Path(out_directory).exists():
       shutil.rmtree(out_directory)
     return make.Popen([
       'git',

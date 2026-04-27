d67ce6c8238f63c31fed2c17ecc1d5f5509cf94d
Marek Rogalski <mafikpl@gmail.com>
2026-04-27 12:34:41 +0200

    Make third_party deps depend on their extension files

--- run_py/git.py
+++ run_py/git.py
@@ -2,16 +2,29 @@
 # SPDX-FileCopyrightText: Copyright 2024 Automat Authors
 # SPDX-License-Identifier: MIT
 
-from functools import partial
+import shutil
+from pathlib import Path
+
 import make
 
 def clone(url, out_directory, tag):
-  '''Return an asynchronous 'git clone' wrapper.'''
-  return partial(make.Popen, [
-    'git',
-    '-c', 'advice.detachedHead=false',
-    '-c', 'core.autocrlf=false',
-    'clone',
-    '--depth', '1',
-    '--branch', tag,
-    url, out_directory])
+  out_directory = Path(out_directory)
+
+  def runner():
+    if out_directory.exists() and (out_directory / '.git').is_dir():
+      return make.Popen([
+        'git', '-C', out_directory,
+        '-c', 'advice.detachedHead=false',
+        'pull', '--depth', '1', '--rebase', '--autostash', 'origin', tag])
+    if out_directory.exists():
+      shutil.rmtree(out_directory)
+    return make.Popen([
+      'git',
+      '-c', 'advice.detachedHead=false',
+      '-c', 'core.autocrlf=false',
+      'clone',
+      '--depth', '1',
+      '--branch', tag,
+      url, out_directory])
+
+  return runner

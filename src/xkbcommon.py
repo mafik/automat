b628e5cd94aba45a47053a852f52345fd3b04fac
Marek Rogalski <mafikpl@gmail.com>
2026-05-27 14:48:28 +0200

    Prevent libxkbcommon from linking against host libicu

--- src/xkbcommon.py
+++ src/xkbcommon.py
@@ -13,10 +13,16 @@ if sys.platform == 'linux':
   hook.ConfigureWithMeson(build.PREFIX / 'lib64' / 'libxkbcommon.a')
   # libxkbcommon mixes C & statically-built C++ dependencies.
   # This confuses Meson. Here we explicitly tell it to link in the C++ stdlib.
-  hook.ConfigureEnvReplaces(LDFLAGS='-lstdc++')
+  #
+  # TODO: Make the PKG_CONFIG_LIBDIR default for all extensions so they don't link against host's libs.
+  hook.ConfigureEnvReplaces(
+    LDFLAGS='-lstdc++',
+    PKG_CONFIG_LIBDIR=':'.join(str(build.PREFIX / x / 'pkgconfig') for x in ('lib64', 'lib', 'share')),
+  )
   hook.ConfigureOption('enable-x11', 'true')
   hook.ConfigureOption('enable-tools', 'false')
   hook.ConfigureOption('enable-xkbregistry', 'false')
+  hook.ConfigureOption('xkb-config-root', '/usr/share/X11/xkb')
   hook.ConfigureDependsOn(xcb.libxcb)
   hook.AddLinkArgs('-l:libxkbcommon.a', '-l:libxkbcommon-x11.a', '-l:libxcb-xkb.a')
   hook.InstallWhenIncluded(r'^xkbcommon/.*\.h$')
